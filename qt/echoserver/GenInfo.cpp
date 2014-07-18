85ac8cebf05b267d654c1440070a20deb414a242
Angelo <langelog@me.com>
2014-07-18 13:51:52 -0400

    fully working

--- qt/echoserver/GenInfo.cpp
+++ qt/echoserver/GenInfo.cpp
@@ -68,6 +68,12 @@ void GenInfo::run() {
         json_stream.clear();
         json_stream.append("[");
         for(int i=0; i<4; i++) {
+            if(i==0) {
+                json_stream.append("{ ");
+            } else {
+                json_stream.append(", { ");
+            }
+
             if(i<g_nHandsCount) {
                 const nite::HandData& handData = hands[i];
                 if(handData.isTracking()) {
@@ -76,32 +82,33 @@ void GenInfo::run() {
                                 handData.getPosition().x,
                                 handData.getPosition().y,
                                 handData.getPosition().z, &x, &y);
-                    json_stream.append("{ \"n\": "+
+                    json_stream.append("\"n\": "+
                                        QString::number(i)+", \"x\": "+
                                        QString::number(x)+", \"y\": "+
                                        QString::number(y)+", \"z\": null}");
                     //cout << "{" << i << "," << handData.getPosition().x << "," << handData.getPosition().y << "," << handData.getPosition().z << "}" << endl;
                     //cout << g_nHandsCount << "|" << i << ": X: " << handData.getPosition().x << ", Y: " << handData.getPosition().y << ", Z: " << handData.getPosition().z << endl;
+                } else {
+                    json_stream.append("\"n\": "+
+                                       QString::number(i)+
+                                       ", \"x\": null, \"y\": null, \"z\": null}");
                 }
             } else {
-                json_stream.append("{ \"n\": "+
+                json_stream.append("\"n\": "+
                                    QString::number(i)+
                                    ", \"x\": null, \"y\": null, \"z\": null}");
                 //cout << "{ \"n\": " << i << ",null,null,NULL}" << endl;
             }
 
-            if(i<3)
-                json_stream.append(",");
-            else {
+            if(i==3) {
                 json_stream.append("]");
-
-                //###############
-                // ENVIAR!!!!:
-                emit enviarInfo(json_stream);
                 //cout << json_stream.toStdString() << endl;
                 //cout << "]" << endl;
             }
         }
+        //###############
+        // ENVIAR!!!!:
+        emit enviarInfo(json_stream);
     }//fin while
 
 //    int i=0;
